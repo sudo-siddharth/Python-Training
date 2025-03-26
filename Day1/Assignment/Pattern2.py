97691bd2bad71cbb2a90ec166cead8e95f563771
Siddharth Patel <siddharthtemporary45@gmail.com>
2025-03-26 09:38:13 +0530

    Day 1 Python Final Submission

--- Day1/Assignment/Pattern2.py
+++ Day1/Assignment/Pattern2.py
@@ -1,31 +1,27 @@
 def print_hollow_square(n):
-    if n <= 0:
+    if n<=0:
         return
     
-    # Create a list to store the numbers in spiral order
     numbers = []
-    for i in range(1, n*n + 1):
+    for i in range(1,n*n+1):
         numbers.append(str(i))
     
-    # Create an n x n matrix filled with spaces
-    matrix = [[' ' for _ in range(n)] for _ in range(n)]
+    matrix=[[' ' for _ in range(n)] for _ in range(n)]
     
-    # Fill the outer layer (border) with numbers in order
-    index = 0
+    index=0
     for i in range(n):
         for j in range(n):
-            if i == 0 or i == n-1 or j == 0 or j == n-1:
-                matrix[i][j] = numbers[index]
-                index += 1
+            if i==0 or i==n-1 or j==0 or j==n-1:
+                matrix[i][j]=numbers[index]
+                index+= 1
     
-    # Print the matrix with the same format
     for i in range(n):
         for j in range(n):
-            if i == 0 or i == n - 1 or j == 0 or j == n - 1:
+            if i==0 or i== n - 1 or j== 0 or j== n - 1:
                 print(str(matrix[i][j]).rjust(2), end=' ')
             else:
-                print('  ', end=' ')
+                print('  ',end=' ')
         print()
 
-n = int(input("Enter the number: "))
+n=int(input("enter the number= "))
 print_hollow_square(n)
